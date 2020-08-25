8948f6f29081ab5000b89a3df44352c12c68e3e4
Tia Firing <tia.firing@nav.no>
2020-08-25 15:19:50 +0200

    unngå at pdfgenerering feiler silently

--- src/main/kotlin/no/nav/syfo/client/PdfgenClient.kt
+++ src/main/kotlin/no/nav/syfo/client/PdfgenClient.kt
@@ -1,12 +1,16 @@
 package no.nav.syfo.client
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.receive
 import io.ktor.client.request.get
+import io.ktor.client.statement.HttpStatement
 import io.ktor.http.ContentType
 import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
 import io.ktor.util.KtorExperimentalAPI
 import no.nav.syfo.helpers.retry
+import no.nav.syfo.log
 import no.nav.syfo.model.Legeerklaering
 import no.nav.syfo.model.PdfModel
 import no.nav.syfo.model.ValidationResult
@@ -17,10 +21,16 @@ class PdfgenClient constructor(
     private val httpClient: HttpClient
 ) {
     suspend fun createPdf(payload: PdfModel): ByteArray = retry("pdfgen") {
-        httpClient.get<ByteArray>(url) {
+        val httpResponse = httpClient.get<HttpStatement>(url) {
             contentType(ContentType.Application.Json)
             method = HttpMethod.Post
             body = payload
+        }.execute()
+        if (httpResponse.status == HttpStatusCode.OK) {
+            httpResponse.call.response.receive<ByteArray>()
+        } else {
+            log.error("Mottok feilkode fra syfopdfgen: {}", httpResponse.status)
+            throw RuntimeException("Mottok feilkode fra syfopdfgen: ${httpResponse.status}")
         }
     }
 }
